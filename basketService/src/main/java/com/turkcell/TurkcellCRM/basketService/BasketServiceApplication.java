3b961462143e2f630e230d45015819eb6555644f
Elnur Akkurt <68028577+111-eln@users.noreply.github.com>
2024-06-12 13:40:47 +0300

    Update BasketServiceApplication.java

--- basketService/src/main/java/com/turkcell/TurkcellCRM/basketService/BasketServiceApplication.java
+++ basketService/src/main/java/com/turkcell/TurkcellCRM/basketService/BasketServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSecurity()
 public class BasketServiceApplication {
 
 	public static void main(String[] args) {
