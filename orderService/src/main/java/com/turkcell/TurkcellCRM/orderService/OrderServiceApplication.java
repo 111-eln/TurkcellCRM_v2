87738ef813de9d038db66d01362fed0a6da727fd
Elnur Akkurt <68028577+111-eln@users.noreply.github.com>
2024-06-12 13:44:08 +0300

    Update OrderServiceApplication.java

--- orderService/src/main/java/com/turkcell/TurkcellCRM/orderService/OrderServiceApplication.java
+++ orderService/src/main/java/com/turkcell/TurkcellCRM/orderService/OrderServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
-//@EnableSecurity
+@EnableSecurity()
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
