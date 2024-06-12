7f36a6ec0c541db1cceb66773ab0cc7561972bc6
Elnur Akkurt <68028577+111-eln@users.noreply.github.com>
2024-06-12 13:19:45 +0300

    Update CatalogServiceApplication.java

--- catalogService/src/main/java/com/turkcell/TurkcellCRM/catalogService/CatalogServiceApplication.java
+++ catalogService/src/main/java/com/turkcell/TurkcellCRM/catalogService/CatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@EnableSecurity()
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
